54a58a3dd0e3add652df20cd0a1f6ebfef38b1f1
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-07-06 11:12:40 +0200

    front setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import CssBaseline from "@mui/material/CssBaseline";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<CssBaseline />
+			<AppBar position="sticky">
+				<Toolbar>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}>
+						<MenuIcon />
+					</IconButton>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1 }}>
+						News
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
