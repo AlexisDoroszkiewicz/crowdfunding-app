f27bfdca40b1d5dded3e37c216accc82e9ca991f
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-07-06 11:36:04 +0200

    nav container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+	AppBar,
+	Button,
+	Container,
+	IconButton,
+	Toolbar,
+	Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,25 +21,29 @@ function MyApp({ Component, pageProps }: AppProps) {
 				/>
 			</Head>
 			<CssBaseline />
+
 			<AppBar position="sticky">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ flexGrow: 1 }}>
-						News
-					</Typography>
-					<Button color="inherit">Login</Button>
-				</Toolbar>
+				<Container maxWidth="lg" disableGutters={true}>
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							sx={{ mr: 2 }}>
+							<MenuIcon />
+						</IconButton>
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{ flexGrow: 1 }}>
+							News
+						</Typography>
+						<Button color="inherit">Login</Button>
+					</Toolbar>
+				</Container>
 			</AppBar>
+
 			<Component {...pageProps} />
 		</>
 	);
